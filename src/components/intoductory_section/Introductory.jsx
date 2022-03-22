ed8066359a61b491b3e326831141482dc8dda759
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-03-22 18:32:22 +0530

    updating intro of home page

--- src/components/intoductory_section/Introductory.jsx
+++ src/components/intoductory_section/Introductory.jsx
@@ -11,13 +11,13 @@ const Introductory = () => {
       <div className='container intro' data-aos="zoom-in">
         <div className='row'>
 
-          <div className='col-xl-6 col-lg-6 video'>
+          <div className='com-md-6 col-xl-6 col-lg-6 video'>
   
               <iframe width="480" height="300" src="https://www.youtube.com/embed/mS60nG6bJwo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           
           </div>
 
-          <div className='col-xl-6 col-lg-6 contentbox'>
+          <div className='col-md-6 col-xl-6 col-lg-6 contentbox'>
               <p>Sponsors : 30+</p>
               <p>Games : 50+</p>
               <p>Participants : 70+</p>
