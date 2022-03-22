45762eaff89126cd0e51d1bab39508d747736376
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-03-22 18:21:42 +0530

    updating home page

--- src/components/intoductory_section/Introductory.jsx
+++ src/components/intoductory_section/Introductory.jsx
@@ -2,12 +2,31 @@ import React from 'react'
 import './introductory.scss'
 
 const Introductory = () => {
-  return (
-    <div className='video' data-aos="zoom-in">
-    <h1>Introduction</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/mS60nG6bJwo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
 
+
+
+  return (
+    <>
+
+      <div className='container intro' data-aos="zoom-in">
+        <div className='row'>
+
+          <div className='col-xl-6 col-lg-6 video'>
+  
+              <iframe width="480" height="300" src="https://www.youtube.com/embed/mS60nG6bJwo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+          </div>
+
+          <div className='col-xl-6 col-lg-6 contentbox'>
+              <p>Sponsors : 30+</p>
+              <p>Games : 50+</p>
+              <p>Participants : 70+</p>
+              <p>Days : 4+</p>
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -27,7 +27,6 @@ const Home = () => {
                 </div>
             </div>
 
-          {/* <div className='calculator'></div> */}
 
             <Introductory/>
             <About
