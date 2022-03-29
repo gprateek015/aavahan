ad370e13541d4df588ed2e9a9386b6f644c5f3f0
Prateek Goyal <gprateek015@gmail.com>
2022-03-29 17:58:06 +0530

    updated events game

--- src/components/event-section/event-section.jsx
+++ src/components/event-section/event-section.jsx
@@ -1,15 +1,21 @@
 import EventCard from '../event-card/event-card';
 import './event-section.scss';
 
-const EventSection = ({ heading }) => {
+const EventSection = ({ heading, events }) => {
+  console.log(events.length);
   return (
     <>
-    <div className='event-section'>
-      <h1 className='heading' style={{ color: 'grey' }}>
-        {heading}
-      </h1>
-      <EventCard sport='Cricket' team1='CSE' team2='IT' time='2:00 pm' />
-    </div>
+      <div className='event-section'>
+        <h1 className='heading' style={{ color: 'grey' }}>
+          {heading}
+        </h1>
+        {/* <EventCard sport='Cricket' team1='CSE' team2='IT' time='12:00 pm' /> */}
+        <div className='events'>
+          {events.map((event, i) => (
+            <EventCard {...event} key={i} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/events/events.jsx
+++ src/pages/events/events.jsx
@@ -6,7 +6,86 @@ import bgmain from '../../assets/bgmain3.png';
 import './events.scss';
 import Footer from '../../components/footer/Footer';
 
+const DJ_Night = ({ heading }) => (
+  <div className='dj-night'>
+    <h1 className='heading' style={{ color: 'grey' }}>
+      {heading}
+    </h1>
+    <div className='EventVideo'>
+      <iframe
+        width='480'
+        height='300'
+        src='https://www.youtube.com/embed/ideEAks0ZF0'
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+);
+
 const EventPage = () => {
+  const events_day1 = [
+    {
+      sport: 'Cricket',
+      team1: 'CSE',
+      team2: 'IT',
+      time: '12:00 pm'
+    },
+    {
+      sport: 'Futsal',
+      team1: 'EE',
+      team2: 'ME',
+      time: '2:00 pm'
+    },
+    {
+      sport: 'Volleyball',
+      team1: 'CIVIL',
+      team2: 'EC',
+      time: '4:00 pm'
+    }
+  ];
+  const events_day2 = [
+    {
+      sport: 'Basketball',
+      team1: 'IP',
+      team2: 'CSE',
+      time: '12:00 pm'
+    },
+    {
+      sport: 'Badminton',
+      team1: 'EE',
+      team2: 'IT',
+      time: '2:00 pm'
+    },
+    {
+      sport: 'Khokho',
+      team1: 'ME',
+      team2: 'CIVIL',
+      time: '4:00 pm'
+    }
+  ];
+  const events_day3 = [
+    {
+      sport: 'Chess',
+      team1: 'CSE',
+      team2: 'CIVIL',
+      time: '12:00 pm'
+    },
+    {
+      sport: 'Table Tennis',
+      team1: 'ME',
+      team2: 'IT',
+      time: '2:00 pm'
+    },
+    {
+      sport: 'BGMI',
+      team1: 'EE',
+      team2: 'IP',
+      time: '4:00 pm'
+    }
+  ];
   return (
     <>
       <div id='Events'></div>
@@ -24,10 +103,10 @@ const EventPage = () => {
         ></iframe>
       </div>
 
-      <EventSection heading='1st Day' />
-      <EventSection heading='2nd Day' />
-      <EventSection heading='3rd Day' />
-      <EventSection heading='DJ Night' />
+      <EventSection heading='1st Day' events={events_day1} />
+      <EventSection heading='2nd Day' events={events_day2} />
+      <EventSection heading='3rd Day' events={events_day3} />
+      <DJ_Night heading='DJ Night' />
 
       <Footer />
     </>
